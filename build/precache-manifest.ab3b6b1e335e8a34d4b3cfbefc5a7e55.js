7cde7182517b7efb5aa8ff044f3c9a9c511346b0
Gordo lee adarmes <jorgeadarmes23@gmail.com>
2020-10-31 22:52:31 -0430

    add build

---
+++ build/precache-manifest.ab3b6b1e335e8a34d4b3cfbefc5a7e55.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f27b4af8e9b34329364dfa8ee47694",
+    "url": "/fitness.git/index.html"
+  },
+  {
+    "revision": "d00ae96919e831c66fb3",
+    "url": "/fitness.git/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "42a73683efc417147231",
+    "url": "/fitness.git/static/css/main.f3dfca2e.chunk.css"
+  },
+  {
+    "revision": "d00ae96919e831c66fb3",
+    "url": "/fitness.git/static/js/2.ebf022d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fitness.git/static/js/2.ebf022d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a73683efc417147231",
+    "url": "/fitness.git/static/js/main.e727cdae.chunk.js"
+  },
+  {
+    "revision": "2379d907ba025a598d1c",
+    "url": "/fitness.git/static/js/runtime-main.c8403ff4.js"
+  },
+  {
+    "revision": "f16a1ad5b4b9c86c0f30cd484308cb3b",
+    "url": "/fitness.git/static/media/404.f16a1ad5.png"
+  },
+  {
+    "revision": "854842fa495e13de2d54968cec61b6d1",
+    "url": "/fitness.git/static/media/500.854842fa.png"
+  },
+  {
+    "revision": "eb728687589f75caecdb7f2b3d71c1d5",
+    "url": "/fitness.git/static/media/circles.eb728687.png"
+  }
+]);
